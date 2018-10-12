a7c4a5db0e74c6f70083cf0d11c636e0f9b2dce4
liyang <1711467488@qq.com>
2018-10-12 09:54:53 +0800

    no message

--- src/menu/aside.js
+++ src/menu/aside.js
@@ -2,12 +2,12 @@
 export default [
   { path: '/index', title: '首页', icon: 'home' },
   {
-    title: '页面',
+    title: '演示页面',
     icon: 'folder-o',
     children: [
-      { path: '/page1', title: '页面 1' },
-      { path: '/page2', title: '页面 2' },
-      { path: '/page3', title: '页面 3' }
+      { path: '/demo/page1', title: '页面 1' },
+      { path: '/demo/page2', title: '页面 2' },
+      { path: '/demo/page3', title: '页面 3' }
     ]
   }
 ]
