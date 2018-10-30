1ae33278a02d982b089de8a514a467dd84b98cce
liyang <1711467488@qq.com>
2018-10-30 20:44:19 +0800

    更新依赖
    Former-commit-id: 93de86ecd6e2bc0408be9167a039ef059efec9e6

--- src/main.js
+++ src/main.js
@@ -12,9 +12,6 @@ import i18n from './i18n'
 // 核心插件
 import d2Admin from '@/plugin/d2admin'
 
-// [ 可选插件组件 ]D2-Crud
-import D2Crud from '@d2-projects/d2-crud'
-
 // 菜单和路由设置
 import router from './router'
 import menuHeader from '@/menu/header'
@@ -23,9 +20,6 @@ import { frameInRoutes } from '@/router/routes'
 
 // 核心插件
 Vue.use(d2Admin)
-
-// 可选插件组件
-Vue.use(D2Crud)
 
 new Vue({
   router,
