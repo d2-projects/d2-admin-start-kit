0b4acf781aaf839c5ef56962e89cd4928b73705e
liyang <1711467488@qq.com>
2018-10-30 20:06:14 +0800

    menu 设置
    Former-commit-id: 29011ec1882cac7ed89433a9112a343be272df6d

--- src/menu/modules/demo-frame.js
+++
@@ -1,8 +0,0 @@
-export default {
-  path: '/demo/frame',
-  title: '内嵌网页',
-  icon: 'globe',
-  children: (pre => [
-    { path: `${pre}d2-doc`, title: 'D2Admin 中文文档', iconSvg: 'd2-admin' }
-  ])('/demo/frame/')
-}
