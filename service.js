c55c41103e69efea56d13957b2f360d61ccc7b4f
han_feng <han_feng@foxmail.com>
2018-11-09 22:14:53 +0800

    调整pkg参数

--- service.js
+++ service.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const compression = require('compression')
 const path = require('path')
 const open = require('open')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'target/dist'))) // 注意这里使用path.join(__dirname, 'dist')而不是'dist'，虽然在命令行中执行起来效果是一样的，不过pkg打包会无法识别到dist目录
+// 注意这里使用path.join(__dirname, 'dist')而不是'dist'，
+// 虽然在命令行中执行起来效果是一样的，不过pkg打包会无法识别到dist目录
+app.use(compression(), express.static(path.join(__dirname, 'target/dist')))
 
 const server = app.listen(8888, '127.0.0.1', function () {
   const host = server.address().address
