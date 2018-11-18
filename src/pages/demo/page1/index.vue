baa7eec818ac66cc5e41e1a98a68d726fdc2146e
han_feng <han_feng@foxmail.com>
2018-11-18 16:34:15 +0800

    纠正乱码问题

--- src/pages/demo/page1/index.vue
+++ src/pages/demo/page1/index.vue
@@ -1,29 +1,29 @@
 <template>
   <d2-container>
-    <h1 class="d2-mt-0">权限测试</h1>
+    <h1 class="d2-mt-0">鏉冮檺娴嬭瘯</h1>
     <el-row class="d2-mb" v-if="checkPermission('/demo/test1')">
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning" v-if="checkPermission('/demo/test2')">警告按钮</el-button>
-      <el-button type="danger" v-if="checkPermission('/demo/test3')">危险按钮</el-button>
+      <el-button>榛樿鎸夐挳</el-button>
+      <el-button type="primary">涓昏鎸夐挳</el-button>
+      <el-button type="success">鎴愬姛鎸夐挳</el-button>
+      <el-button type="info">淇℃伅鎸夐挳</el-button>
+      <el-button type="warning" v-if="checkPermission('/demo/test2')">璀﹀憡鎸夐挳</el-button>
+      <el-button type="danger" v-if="checkPermission('/demo/test3')">鍗遍櫓鎸夐挳</el-button>
     </el-row>
     <el-row class="d2-mb"  v-if="checkPermission('/demo/test2')">
-      <el-button plain>朴素按钮</el-button>
-      <el-button type="primary" plain>主要按钮</el-button>
-      <el-button type="success" plain>成功按钮</el-button>
-      <el-button type="info" plain>信息按钮</el-button>
-      <el-button type="warning" plain>警告按钮</el-button>
-      <el-button type="danger" plain v-if="checkPermission('/demo/test3')">危险按钮</el-button>
+      <el-button plain>鏈寸礌鎸夐挳</el-button>
+      <el-button type="primary" plain>涓昏鎸夐挳</el-button>
+      <el-button type="success" plain>鎴愬姛鎸夐挳</el-button>
+      <el-button type="info" plain>淇℃伅鎸夐挳</el-button>
+      <el-button type="warning" plain>璀﹀憡鎸夐挳</el-button>
+      <el-button type="danger" plain v-if="checkPermission('/demo/test3')">鍗遍櫓鎸夐挳</el-button>
     </el-row>
     <el-row class="d2-mb" v-if="checkPermission('/demo/test3')">
-      <el-button round>圆角按钮</el-button>
-      <el-button type="primary" round>主要按钮</el-button>
-      <el-button type="success" round>成功按钮</el-button>
-      <el-button type="info" round>信息按钮</el-button>
-      <el-button type="warning" round>警告按钮</el-button>
-      <el-button type="danger" round>危险按钮</el-button>
+      <el-button round>鍦嗚鎸夐挳</el-button>
+      <el-button type="primary" round>涓昏鎸夐挳</el-button>
+      <el-button type="success" round>鎴愬姛鎸夐挳</el-button>
+      <el-button type="info" round>淇℃伅鎸夐挳</el-button>
+      <el-button type="warning" round>璀﹀憡鎸夐挳</el-button>
+      <el-button type="danger" round>鍗遍櫓鎸夐挳</el-button>
     </el-row>
   </d2-container>
 </template>
