1be47ad15b1f8742102665da0c48698c9045e36e
han_feng <han_feng@foxmail.com>
2019-02-13 17:42:36 +0800

    完善模块依赖关系展示

--- src/modular/view/graph.vue
+++ src/modular/view/graph.vue
@@ -30,18 +30,40 @@ export default {
       graph.setEdgeLabelsMovable(false) // 连线标签是否可移动
       graph.setAllowDanglingEdges(false) // 连线是否可以悬空
       graph.setCellsDisconnectable(false) // 是否允许断开连接
+      // graph.setDisconnectOnMove(false) // 移动连线是是否可以断开连接
+      // graph.setCellsLocked(true)
 
       // graph.setAutoSizeCells(true)
       graph.setPanning(true) // 右键移动坐标轴
       graph.setResizeContainer(false) // 画布是否随内容调整大小
       graph.setEnabled(true) // false 为只读模式
 
-      // 更改风格的样式
-      const style = graph.getStylesheet().getDefaultVertexStyle()
+      // 更改默认样式
+      let style = graph.getStylesheet().getDefaultVertexStyle()
+      style[mx.mxConstants.STYLE_SHAPE] = mx.mxConstants.SHAPE_RECTANGLE
+      style[mx.mxConstants.STYLE_PERIMETER] = mx.mxPerimeter.RectanglePerimeter
+      style[mx.mxConstants.STYLE_ROUNDED] = true
+      style[mx.mxConstants.STYLE_GRADIENTCOLOR] = 'white'
+      style[mx.mxConstants.STYLE_FONTSIZE] = '14'
+
+      style = graph.getStylesheet().getDefaultEdgeStyle()
+      style[mx.mxConstants.STYLE_ROUNDED] = true // 圆角连线
+      style[mx.mxConstants.STYLE_EDGE] = mx.mxEdgeStyle.ElbowConnector // 自己拐弯的连线
+
+      // 创建扩展点样式
+      style = {}
       style[mx.mxConstants.STYLE_SHAPE] = mx.mxConstants.SHAPE_ELLIPSE
       style[mx.mxConstants.STYLE_PERIMETER] = mx.mxPerimeter.EllipsePerimeter
-      style[mx.mxConstants.STYLE_GRADIENTCOLOR] = 'white'
-      style[mx.mxConstants.STYLE_FONTSIZE] = '12'
+      style[mx.mxConstants.STYLE_FONTCOLOR] = 'black'
+      style[mx.mxConstants.STYLE_FILLCOLOR] = 'white'
+      // style[mx.mxConstants.STYLE_GRADIENT_DIRECTION] = mx.mxConstants.DIRECTION_NORTH
+      style[mx.mxConstants.STYLE_FONTSIZE] = '10'
+      graph.getStylesheet().putCellStyle('extensionPoint', style)
+
+      // 扩展依赖关系样式
+      style = {}
+      style[mx.mxConstants.STYLE_EDGE] = mx.mxEdgeStyle.SegmentConnector
+      graph.getStylesheet().putCellStyle('extension', style)
 
       // Enables rubberband selection
       // new mx.mxRubberband(graph)
@@ -55,14 +77,38 @@ export default {
       try {
         const w = 120
         const h = 40
+        const ew = 90
+        const eh = 20
 
         const modules = window.$modular.modules
         const vertexs = {}
+        const points = {}
         modules.forEach(module => {
           const name = module.name
-          if (name === 'modular-core') return
-          const v = graph.insertVertex(parent, null, name, 0, 0, w, h)
+          let style = null
+          if (name === 'modular-core') {
+            style = 'fillColor=green'
+          }
+          const v = graph.insertVertex(parent, null, name, 20, 20, w, h, style)
           vertexs[name] = v
+          if (module.extensionPoints) {
+            const ps = module.extensionPoints
+            for (let key in ps) {
+              const p = graph.insertVertex(parent, null, key, 20, 20, ew, eh, 'extensionPoint')
+              graph.insertEdge(parent, null, '', p, v, 'extension')
+              points[key] = p
+            }
+          }
+          if (module.extensions) {
+            const ext = module.extensions
+            for (let key in ext) {
+              if (points[key]) {
+                graph.insertEdge(parent, null, '', v, points[key], 'extension')
+              } else {
+                console.log('extensionPoints 定义不存在' + key, ext[key], module)
+              }
+            }
+          }
           if (module.dependencies && module.dependencies.length) {
             const ds = module.dependencies
             ds.forEach(d => {
@@ -75,18 +121,22 @@ export default {
           }
         })
 
+        // style = 'shape=ellipse;perimeter=ellipsePerimeter'
+
         // const circleLayout = new mx.mxCircleLayout(graph)
 
         // const fastOrganicLayout = new mx.mxFastOrganicLayout(graph)
         // fastOrganicLayout.forceConstant = 80
 
         // const hierarchicalLayout = new mx.mxHierarchicalLayout(graph, mx.mxConstants.DIRECTION_WEST)
+        const hierarchicalLayout = new mx.mxHierarchicalLayout(graph, mx.mxConstants.DIRECTION_NORTH)
+        hierarchicalLayout.interRankCellSpacing = 50
 
-        const compactTreeLayout = new mx.mxCompactTreeLayout(graph, false)
-        compactTreeLayout.useBoundingBox = false
-        compactTreeLayout.edgeRouting = false
-        compactTreeLayout.levelDistance = 30
-        compactTreeLayout.nodeDistance = 30
+        // const compactTreeLayout = new mx.mxCompactTreeLayout(graph, false) // 紧凑树布局算法，仅适用于单根节点情况
+        // compactTreeLayout.useBoundingBox = false
+        // compactTreeLayout.edgeRouting = false
+        // compactTreeLayout.levelDistance = 30
+        // compactTreeLayout.nodeDistance = 30
 
         // const edgeLabelLayout = new mx.mxEdgeLabelLayout(graph)
 
@@ -100,8 +150,8 @@ export default {
         let layout
         // layout = circleLayout
         // layout = fastOrganicLayout
-        // layout = hierarchicalLayout
-        layout = compactTreeLayout
+        layout = hierarchicalLayout
+        // layout = compactTreeLayout
         // layout = edgeLabelLayout
         // layout = compositeLayout
         layout.execute(parent)
