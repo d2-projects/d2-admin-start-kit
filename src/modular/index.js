d7da2a05dfde8294e40c89f04e9cb385c8fe87b8
han_feng <han_feng@foxmail.com>
2019-02-18 22:08:41 +0800

    完善模块化实现

--- src/modular/index.js
+++ src/modular/index.js
@@ -1,33 +1,52 @@
+class ModulesLoader {
+  constructor () {
+    this.modules = []
+    this.nameMap = {}
+  }
+  add (module) {
+    if (!this.contains(module)) {
+      this.nameMap[module.name] = module
+      this.modules.push(module)
+    }
+  }
+  getModules () {
+    return this.modules
+  }
+  contains (module) {
+    return !!this.nameMap[module.name]
+  }
+}
 // 模块化应用实现类
 export default class Modular {
+  // 构造函数
   constructor (config) {
     config = config || {}
     let modules = config.modules || []
     this.application = config.application || {}
     this.strict = config.strict
+    this.errors = []
 
     // 建立 name 查询索引
     const nameMapping = {}
     modules.forEach(module => {
       if (module.name === undefined) {
-        console.log('模块名称未定义', module)
+        this._log('模块名称未定义', module)
         return
       }
       const name = module.name
       if (nameMapping[name]) {
-        console.log('模块名称重复', nameMapping[name], module)
+        this._log('模块名称重复', nameMapping[name], module)
         return
       }
       nameMapping[name] = module
     })
 
     // 解析依赖，模块排序
-    const sortModules = []
-    sortModules.push(nameMapping['modular-core'])
+    const modulesLoader = new ModulesLoader()
+    modulesLoader.add(nameMapping['modular-core'])
 
     function fillDepens (item) {
-      if (sortModules.find(m => m.name === item.name)) {
-        // FIXME 循环查找，存在性能问题
+      if (modulesLoader.contains(item)) {
         return true
       }
       if (item.dependencies && item.dependencies.length) {
@@ -35,33 +54,33 @@ export default class Modular {
         const len = ds.length
         for (let i = 0; i < len; i++) {
           const d = ds[i]
-          if (sortModules.find(m => m.name === d) === undefined) {
-            // FIXME 循环查找，存在性能问题
+          if (!modulesLoader.contains(item)) {
             // 依赖模块未解析
             if (nameMapping[d]) {
               if (fillDepens(nameMapping[d])) {
                 // 依赖项加载成功
                 continue
               } else {
-                console.log('“' + item.name + '”依赖的模块“' + d + '”解析失败')
+                this._log('“' + item.name + '”依赖的模块“' + d + '”解析失败')
                 return false
               }
             } else {
-              console.log('“' + item.name + '”依赖的模块“' + d + '”不存在')
+              this._log('“' + item.name + '”依赖的模块“' + d + '”不存在')
               return false
             }
           }
         }
         // 依赖项全部加载成功
       }
-      sortModules.push(item)
+      modulesLoader.add(item)
       return true
     }
     modules.forEach(module => {
       fillDepens(module)
     })
-    this.modules = sortModules
-    modules = sortModules
+
+    modules = modulesLoader.getModules()
+    this.modules = modules
 
     // 组装扩展配置
     const points = {}
@@ -71,7 +90,7 @@ export default class Modular {
         const ps = module.extensionPoints
         for (let key in ps) {
           if (points[key]) {
-            console.log('重复的 extensionPoints 定义 ' + key, points[key], module)
+            this._log('重复的 extensionPoints 定义 ' + key, points[key], module)
           } else {
             points[key] = { module: module.name, config: ps[key] }
           }
@@ -84,7 +103,7 @@ export default class Modular {
             extens[key] = extens[key] || {}
             Object.assign(extens[key], ext[key])
           } else {
-            console.log('extensionPoints 定义不存在' + key, ext[key], module)
+            this._log('extensionPoints 定义不存在 ' + key, ext[key], module)
           }
         }
       }
@@ -92,12 +111,17 @@ export default class Modular {
     this.extensionPoints = points
     this.extensions = extens
   }
+  // 启动模块化应用
   start () {
-    console.log('Start Modules >>>>>>')
     this.modules.forEach(module => {
       if (module.activator && module.activator.start) {
         module.activator.start(this)
       }
     })
+  }
+  // 记录日志
+  _log () {
+    this.errors.push(arguments)
+    console.log(arguments)
   }
 }
