f8439300b0e94fc7f983de902e518a39f4a232d0
liyang <1711467488@qq.com>
2019-05-07 09:24:56 +0800

    clean

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: [
-    '@vue/app'
-  ],
-  plugins: [
-    // https://babeljs.io/docs/en/babel-plugin-transform-runtime/
-    '@babel/plugin-transform-runtime'
-  ]
-}
