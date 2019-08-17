a50ec9aeb8963f631b22a94620ebd25a2e00d15f
FairyEver <1711467488@qq.com>
2019-08-17 17:59:00 +0800

    删除7月份的广告展示
    Former-commit-id: 60856c5a938a159cddbc58b3b0c4e6ca35e8efdf

--- src/menu/header.js
+++ src/menu/header.js
@@ -9,13 +9,5 @@ export default [
       { path: '/page2', title: '页面 2' },
       { path: '/page3', title: '页面 3' }
     ]
-  },
-  // 恰饭
-  ...process.env.VUE_APP_BUILD_MODE === 'TRAVIS' ? [
-    {
-      path: 'https://gio.ren/w/nP2OALom',
-      title: 'Web进阶教程',
-      icon: 'book'
-    }
-  ] : []
+  }
 ]
