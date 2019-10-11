62e1139aa98e80384711e2d9c767b09cd49c3e7e
FairyEver <1711467488@qq.com>
2019-10-11 10:28:26 +0800

    更新文档地址

--- src/views/system/index/page.vue
+++ src/views/system/index/page.vue
@@ -5,7 +5,7 @@
       <template slot="footer">
         <div class="btn-group">
           <span class="btn-group__btn" @click="$open('https://github.com/d2-projects')">开源组织</span> |
-          <span class="btn-group__btn" @click="$open('https://doc.d2admin.fairyever.com/zh/')">文档</span> |
+          <span class="btn-group__btn" @click="$open('https://fairyever.com/d2-admin/doc//zh/')">文档</span> |
           <span class="btn-group__btn" @click="$open('https://github.com/d2-projects/d2-admin-start-kit')">简化版</span> |
           <span class="btn-group__btn" @click="$open('https://alibaba.github.io/ice/scaffold?type=vue')">飞冰</span> |
           <span class="btn-group__btn" @click="$open('https://juejin.im/user/57a48b632e958a006691b946/posts')">掘金</span> |
