f9f06074303b9be4f25b691df15cdc226342b97e
FairyEver <1711467488@qq.com>
2020-05-08 10:41:10 +0800

    feat: update 1.11.0

--- src/api/service.js
+++ src/api/service.js
@@ -1,7 +1,9 @@
-import store from '@/store'
 import axios from 'axios'
+import Adapter from 'axios-mock-adapter'
 import { Message } from 'element-ui'
+import { get } from 'lodash'
 import util from '@/libs/util'
+import store from '@/store'
 
 // 创建一个错误
 function errorCreate (msg) {
@@ -34,20 +36,11 @@ function errorLog (error) {
 }
 
 // 创建一个 axios 实例
-const service = axios.create({
-  baseURL: process.env.VUE_APP_API,
-  timeout: 5000 // 请求超时时间
-})
+const service = axios.create()
 
 // 请求拦截器
 service.interceptors.request.use(
-  config => {
-    // 在请求发送之前做一些处理
-    const token = util.cookies.get('token')
-    // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
-    config.headers['X-Token'] = token
-    return config
-  },
+  config => config,
   error => {
     // 发送失败
     console.log(error)
@@ -105,4 +98,18 @@ service.interceptors.response.use(
   }
 )
 
-export default service
+export const mock = new Adapter(service)
+
+export function request (config) {
+  const token = util.cookies.get('token')
+  const configDefault = {
+    headers: {
+      Authorization: token,
+      'Content-Type': get(config, 'headers.Content-Type', 'application/json')
+    },
+    timeout: 5000,
+    baseURL: process.env.VUE_APP_API,
+    data: {}
+  }
+  return service(Object.assign(configDefault, config))
+}
--- src/plugin/error/index.js
+++ src/plugin/error/index.js
@@ -1,28 +1,54 @@
+import { get, isObject } from 'lodash'
 import store from '@/store'
 import util from '@/libs/util'
 
 export default {
   install (Vue, options) {
-    Vue.config.errorHandler = function (error, instance, info) {
-      Vue.nextTick(() => {
-        // store 追加 log
-        store.dispatch('d2admin/log/push', {
-          message: `${info}: ${error.message}`,
-          type: 'danger',
-          meta: {
-            error,
-            instance
-          }
-        })
-        // 只在开发模式下打印 log
-        if (process.env.NODE_ENV === 'development') {
-          util.log.capsule('D2Admin', 'ErrorHandler', 'danger')
+    function writeLog (logType) {
+      return (error, vm, info = '') => {
+        Vue.nextTick(() => {
+          store.dispatch('d2admin/log/push', {
+            message: `${info}: ${isObject(error) ? error.message : error}`,
+            type: logType,
+            meta: {
+              error,
+              vm
+            }
+          })
+          if (process.env.NODE_ENV !== 'development') return
+          util.log.capsule('D2Admin', 'ErrorHandler', logType)
           util.log.danger('>>>>>> 错误信息 >>>>>>')
           console.log(info)
           util.log.danger('>>>>>> Vue 实例 >>>>>>')
-          console.log(instance)
+          console.log(vm)
           util.log.danger('>>>>>> Error >>>>>>')
           console.log(error)
+        })
+      }
+    }
+    if (process.env.NODE_ENV === 'development') {
+      Vue.config.warnHandler = writeLog('warning')
+    }
+    Vue.config.errorHandler = writeLog('danger')
+    window.onunhandledrejection = error => {
+      store.dispatch('d2admin/log/push', {
+        message: get(error, 'reason.message', 'Unknown error'),
+        type: 'danger',
+        meta: {
+          error: get(error, 'reason'),
+          trace: get(error, 'reason.stack')
+        }
+      })
+    }
+    window.onerror = (event, source, lineno, colno, error) => {
+      store.dispatch('d2admin/log/push', {
+        message: get(error, 'message', 'Unknown error'),
+        type: 'danger',
+        meta: {
+          error,
+          trace: get(error, 'stack'),
+          source: `${source}@${lineno}:${colno}`,
+          event: event
         }
       })
     }
